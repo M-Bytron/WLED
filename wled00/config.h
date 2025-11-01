9b05b2b3cf8abfff2043d251dbf444336306a1bd
mahdidnp <mahdidinpazhouh@gmail.com>
2025-11-01 17:37:25 +0330

    setted to Banbootec

--- wled00/config.h
+++ wled00/config.h
@@ -5,13 +5,10 @@
 #ifndef WLED_CONFIG_H
 #define WLED_CONFIG_H
 
-#define STATIC_SSID "Xiaomi 11T Pro"
-#define STATIC_PASS "123456789"
+#define STATIC_SSID "Bambootec"
+#define STATIC_PASS "12345678"
 
 #define MY_LED_PINS 5,18,19,21
 #define LED_REVERSED false
-
-#define STATIC_PASS "123456789"
-
 
 #endif //WLED_CONFIG_H
