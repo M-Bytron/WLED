20ab572caffba5c4f6ee1dca076751ee8b8dcf7b
alirezatooghi <96327370+alirezatooghi@users.noreply.github.com>
2025-11-03 13:50:17 +0330

    config for Network, GPIO, DMX
    Network Credentials, PWM RGBW GPIO, DMX Input added to Config.h

--- wled00/config.h
+++ wled00/config.h
@@ -10,11 +10,11 @@
 #define STATIC_PASS "12345678"
 #define DMX_INPUT_RX_PIN    33
 #define DMX_INPUT_TX_PIN    17
-#define DMX_INPUT_EN_PIN    -1  // we dont use RS485
-#define DMX_INPUT_PORT  2   // UART2
+#define DMX_INPUT_EN_PIN    -1  // we dont use RS485 module
+#define DMX_INPUT_PORT  1   // DMX Universe
 
-#define MY_LED_PINS 5,18,19,21
-#define LED_REVERSED false
+#define MY_LED_PINS 5,18,19,21  // Red, Green, Blue, White
+#define LED_REVERSED    false
 
 
 #endif //WLED_CONFIG_H
