1d0c16ac0cfd86e68873e8a6de975c0b68f972bb
alirezatooghi <96327370+alirezatooghi@users.noreply.github.com>
2025-11-03 14:27:50 +0330

    WiFi, GPIO, DMX configs are tested
    DMX receiver not working yet

--- wled00/config.h
+++ wled00/config.h
@@ -6,14 +6,29 @@
 #ifndef WLED_CONFIG_H
 #define WLED_CONFIG_H
 
-#define STATIC_SSID "Bambootec"
-#define STATIC_PASS "12345678"
-#define DMX_INPUT_RX_PIN    33
-#define DMX_INPUT_TX_PIN    17
-#define DMX_INPUT_EN_PIN    -1  // we dont use RS485 module
-#define DMX_INPUT_PORT  1   // DMX Universe
 
-#define MY_LED_PINS 5,18,19,21  // Red, Green, Blue, White
+// ---------------------------------
+// ------ Network Credentials ------
+#define STATIC_SSID     "Bambootec"
+#define STATIC_PASS     "12345678"
+
+// ---------------------------------
+// ------ DMX Receiver Config ------
+#define DMX_INPUT_RX_PIN    33    // Based on Harald Circuit
+#define DMX_INPUT_TX_PIN    25
+#define DMX_INPUT_EN_PIN    -1   // we dont use RS485 module
+#define DMX_INPUT_PORT      1    // DMX Universe
+
+// ---------------------------------
+// ---------- LED Config -----------
+// -> PWM RGBW
+#define MY_LED_TYPE     TYPE_ANALOG_4CH
+#define MY_LED_PINS     22,21,17,16  // Red, Green, Blue, White (Based on Harald Circuit)
+
+// -> WS28*1x
+// #define MY_LED_TYPE     TYPE_WS2812_RGB
+// #define MY_LED_PINS     16  
+
 #define LED_REVERSED    false
 
 
