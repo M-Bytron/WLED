862dc04fb23a8bd5c659484b019a41cd5620f561
mahdidnp <mahdidinpazhouh@gmail.com>
2025-11-05 18:03:20 +0330

    tempurture sesnsor configs

--- wled00/config.h
+++ wled00/config.h
@@ -9,12 +9,14 @@
 #define STATIC_SSID "Bambootec"
 #define STATIC_PASS "12345678"
 #define DMX_INPUT_RX_PIN    33
-#define DMX_INPUT_TX_PIN    17
+#define DMX_INPUT_TX_PIN    25
 #define DMX_INPUT_EN_PIN    -1  // we dont use RS485
-#define DMX_INPUT_PORT  2   // UART2
+#define DMX_INPUT_PORT  1   //
 
-#define MY_LED_PINS 5,18,19,21
+#define MY_LED_PINS 5,21,17,16   // Red, Green, Blue, White
 #define LED_REVERSED false
 
+#define SENSOR_PIN 4
+#define ADC_OFFSET 0.23
 
 #endif //WLED_CONFIG_H
