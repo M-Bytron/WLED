179b3a60396fbe9bd7e8d0e9547a09117d07959e
alirezatooghi <96327370+alirezatooghi@users.noreply.github.com>
2025-11-19 11:05:13 +0330

    Code Refactored - DMX Shifting Fixed

--- wled00/MBytron_DMX.cpp
+++ wled00/MBytron_DMX.cpp
@@ -6,67 +6,58 @@
 #include "wled.h"
 #include <Arduino.h>
 #include <cstddef>
-// #include "set.cpp"
 
-// ----------------------
+// -----------------------
 // ------- Globals -------
-int enablePin = 21;
 
-/////////////////////////////////////
-volatile uint64_t dmx_start_byte_received = 0;
-volatile uint64_t mab_duration = 0;
-uart_config_t uart_config;
-//////////////////////////////////////////
 
 static const char *TAG = "DMX_RX";
 
 // ---- UART Configs
-#define DMX_RX_PIN ((gpio_num_t)DMX_RX)
-#define DMX_TX_PIN DMX_TX
-
-// #define DMX_FRAME_SIZE 513 // 1 start code + 512 channels
+#define DMX_RX_PIN        ((gpio_num_t)DMX_RX)
+#define DMX_TX_PIN        DMX_TX
 #define DMX_BAUD_RATE     250000
 #define DMX_RX_BUF_SIZE   DMX_FRAME_SIZE
 #define DMX_TIMEOUT_MS    200
 
-QueueHandle_t uart_queue;
-
 // ---- DMX variables
 #define dmx_data_queue_size 10
 #define required_dmx_data_size 4
+
 uint8_t dmx_frame[DMX_FRAME_SIZE];
 bool dmxIsConnected = false;
 unsigned long dmxlastUpdate = millis();
-unsigned long dmxLastPacketTime;
-QueueHandle_t dmx_data_queue;
-uint32_t data_couter = 0;
 
 typedef struct {
   uint8_t data[required_dmx_data_size];
 } dmx_short_frame_t;
 dmx_short_frame_t frame_to_send; 
+QueueHandle_t dmx_data_queue;
+SemaphoreHandle_t dmx_break_semaphore; // used to signal from ISR to task
 
+// ---- Counters
+volatile int16_t extra_break = 0;
+uint32_t data_couter = 0;
+
+// ----- UART FIFO Data
 size_t fifo_B_bef_strtr_svng;
 size_t fifo_B_aft_break;
 size_t fifo_B_bef_clr;
 
 // ---- Break Variable
-volatile bool break_detected = false;
 volatile uint64_t break_start_time = 0;
 volatile uint64_t break_end_time = 0;
 volatile uint32_t break_width_us = 0;
+volatile uint64_t mab_duration = 0;
+
+// ---- Flags
+volatile bool break_detected = false;
 volatile bool ready_to_receive = false;
 volatile bool wait_to_start_dmx_data = false;
-volatile int16_t bytes_in_fifo = 0;
-volatile int16_t byte_before_start_saving = 0;
-volatile int16_t byte_before_clearing = 0;
-volatile int16_t extra_break = 0;
 
-SemaphoreHandle_t dmx_break_semaphore; // used to signal from ISR to task
-
-// ---------------------------------------------------
-// ------- General Functions -------------------------
-// ---------------------------------------------------
+// ----------------------------------------------------------------------------
+// ------- General Functions --------------------------------------------------
+// ----------------------------------------------------------------------------
 
 // ---------- set RGBW values directly ----------
 void setRGBWValues(byte r, byte g, byte b, byte w) {
@@ -91,6 +82,7 @@ void DMX_Setup(int dmx_num, int TX_PIN, int RX_PIN) {
   DEBUG_PRINTF(">  DMX Setup <\n");
   DEBUG_PRINTF("--------------\n");
 
+  uart_config_t uart_config;
   uart_config = {.baud_rate = DMX_BAUD_RATE,
                  .data_bits = UART_DATA_8_BITS,
                  .parity = UART_PARITY_DISABLE,
@@ -101,8 +93,8 @@ void DMX_Setup(int dmx_num, int TX_PIN, int RX_PIN) {
   uart_param_config(dmx_num, &uart_config);
   uart_set_pin(dmx_num, DMX_TX, DMX_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 
-  // Install UART driver with event queue
-  // QueueHandle_t uart_queue;
+  // ---- Install UART driver with event queue
+  QueueHandle_t uart_queue;
   uart_driver_install(dmx_num, DMX_RX_BUF_SIZE, 0, 20, &uart_queue, 0);
   DEBUG_PRINTF("DMX UART RX task started!\n");
 }
@@ -112,25 +104,26 @@ static void IRAM_ATTR dmx_rx_isr_handler(void *arg) {
 
   int level = gpio_get_level(DMX_RX_PIN);
   uint64_t now = esp_timer_get_time(); // in microseconds
-  if (level == 0) {
-    // Falling edge → start of break
+
+  // ---- Falling edge → start of break
+  if (level == 0) {    
     break_detected = true;
     break_start_time = now;
-  } else {
-    // Rising edge → end of break
+  } 
+  // ---- Rising edge → end of break
+  else {    
     if (break_detected) {
       break_end_time = now;
       break_width_us = (uint32_t)(break_end_time - break_start_time);
       break_detected = false;
 
-      // Valid DMX break is typically >88 µs
+      // ---- Valid DMX break is typically >88 µs
       if (break_width_us > 88) {
         if (!ready_to_receive) {          
           uart_get_buffered_data_len(DMX_UART_NUM, &fifo_B_aft_break);
-          // bytes_in_fifo = fifo_B_aft_break;
           uart_flush_input(DMX_UART_NUM);
           ready_to_receive = true;
-          // wait_to_start_dmx_data = true;
+          /* wait_to_start_dmx_data = true; */
           BaseType_t xHigherPriorityTaskWoken = pdFALSE;
           xSemaphoreGiveFromISR(dmx_break_semaphore, &xHigherPriorityTaskWoken);
           if (xHigherPriorityTaskWoken)
@@ -174,9 +167,6 @@ void dmx_uart_rx_task(void *pvParameters) {
   uart_event_t event;
   int len;
   bool break_detected = false;
-  // uint32_t break_start_time = 0;
-  uint8_t trash = 0;
-  size_t buffer_uart_rx = 0;
 
   while (1) {
 
@@ -185,7 +175,6 @@ void dmx_uart_rx_task(void *pvParameters) {
       if (!dmxIsConnected)
         dmxIsConnected = true;      
       uart_get_buffered_data_len(DMX_UART_NUM, &fifo_B_bef_strtr_svng);
-      // byte_before_start_saving = fifo_B_bef_strtr_svng;
       int len = uart_read_bytes(DMX_UART_NUM, dmx_frame, DMX_FRAME_SIZE,
                                 50 / portTICK_PERIOD_MS);
       // -- valid DMX data
@@ -202,73 +191,13 @@ void dmx_uart_rx_task(void *pvParameters) {
         DEBUG_PRINTF("Incomplete DMX frame (%d bytes)\n", len);
       }      
       uart_get_buffered_data_len(DMX_UART_NUM, &fifo_B_bef_clr);
-      // byte_before_clearing = fifo_B_bef_clr;
-      // if (byte_before_clearing > 1) {
-      //   esp_err_t err = uart_flush_input(DMX_UART_NUM);
-      //   if (err != ESP_OK) {
-      //     DEBUG_PRINTF("UART flush failed: %d\n", err);
-      //   }
-      // }
       ready_to_receive = false;
     }
-
-    // Wait for UART events
-    /*if (xQueueReceive(uart_queue, (void *)&event, portMAX_DELAY)) {
-
-      // if (dmx_receiving_data == false) {
-        switch (event.type) {
-
-        case UART_BREAK:
-          break_detected = true;
-          break_start_time = millis();
-          // DEBUG_PRINTF("BREAK detected at %lu\n", break_start_time);
-
-          uart_read_bytes(DMX_UART_NUM, &trash, 1, 10 / portTICK_PERIOD_MS);
-          // ESP_ERROR_CHECK(uart_get_buffered_data_len(DMX_UART_NUM,
-    (size_t*)&buffer_uart_rx));
-          // DEBUG_PRINTF("BREAK: after Buffered data: %u\n", buffer_uart_rx);
-          break;
-
-          default:
-          case UART_DATA:
-          if (break_detected) {
-            // Measure MAB duration (time between BREAK end and data start)
-            uint32_t current_time = millis();
-            mab_duration = current_time - break_start_time;
-            // DEBUG_PRINTF("MAB duration: %lu us\n", mab_duration);
-
-            // Read the DMX frame
-            int len = uart_read_bytes(DMX_UART_NUM, dmx_frame, DMX_FRAME_SIZE-1,
-    50 / portTICK_PERIOD_MS); strip.suspend(); // Block strip servicing
-            setRGBWValues(dmx_frame[DMXAddress], dmx_frame[DMXAddress],
-    dmx_frame[DMXAddress], dmx_frame[DMXAddress]);
-            // -- Queue DMX to print it in another task
-            #ifdef Print_DMX
-            if (dmx_data_queue) {
-              data_couter++;
-              dmx_short_frame_t frame_to_send;
-              memcpy(frame_to_send.data, dmx_frame + DMXAddress + 1,
-    sizeof(frame_to_send.data)); if (xQueueSend(dmx_data_queue, &frame_to_send,
-    0) != pdTRUE ) // sends first 6 bytes (array decays to pointer)
-                DEBUG_PRINTF("Failed to queue\n");
-            }
-            #endif
-
-            break_detected = false;
-          }
-          break;
-
-        // default:
-          // DEBUG_PRINTF("default\n");
-          // break;
-        }
-
-    }*/
   }
 }
 
 // ------- Print DMX Data Task -------
-void print_dmx_data(void *pvParameters) {
+void process_dmx_data_task(void *pvParameters) {
 
   dmx_short_frame_t received_frame;
   dmx_short_frame_t previous_frame;
@@ -277,35 +206,38 @@ void print_dmx_data(void *pvParameters) {
 
     if (xQueueReceive(dmx_data_queue, &received_frame, portMAX_DELAY)) {
 
-      strip.suspend(); // Block strip servicing
+      // ----- Block strip servicing
+      strip.suspend(); 
+      
+      // ---- Process DMX data -----      
       setRGBWValues(received_frame.data[0], received_frame.data[1],
                     received_frame.data[2], received_frame.data[3]);
+
+      // ---- Print DMX data -----
       #ifdef Print_DMX
-      // if (memcmp(&previous_frame, &received_frame, sizeof(dmx_short_frame_t))
-      // != 0){
-      //   previous_frame = received_frame;
-      ////////////////////////////////////////////
-      // DEBUG_PRINTF("BREAK detected at %lu\n", break_start_time);
-      // DEBUG_PRINTF("MAB duration: %lu us\n", mab_duration);
-      ////////////////////////////////////////////
-      DEBUG_PRINTF("Bytes after detection break: %u\n", fifo_B_aft_break);
-      DEBUG_PRINTF("Bytes before start saving: %u\n", fifo_B_bef_strtr_svng);
-      DEBUG_PRINTF("Bytes before clearing: %u\n", fifo_B_bef_clr);
-      DEBUG_PRINTF("extra_break: %u\n", extra_break);
-      // DEBUG_PRINTF("Valid break detected: %lu us\n", (unsigned
-      // long)break_width_us); DEBUG_PRINTF("MAB duration: %.2f us\n",
-      // mab_duration/1000.0); DEBUG_PRINTF("Data Couter: %ld\n", data_couter);
-      // DEBUG_PRINTF(" - Red: %02X \n - Green: %02X \n - Blue: %02X \n - White:
-      // %02X\n",
-      //   received_frame.data[0], received_frame.data[1],
-      //   received_frame.data[2], received_frame.data[3]);
-      // DEBUG_PRINTF("First 6 DMX bytes: ");
-      // for (int i = 0; i < 100; i++) {
-      //   DEBUG_PRINTF("%02X ", dmx_frame[i]);
-      // }
+      if (memcmp(&previous_frame, &received_frame, sizeof(dmx_short_frame_t))
+      != 0){
+        previous_frame = received_frame;      
+      
+      // DEBUG_PRINTF("Bytes after detection break: %u\n", fifo_B_aft_break);
+      // DEBUG_PRINTF("Bytes before start saving: %u\n", fifo_B_bef_strtr_svng);
+      // DEBUG_PRINTF("Bytes before clearing: %u\n", fifo_B_bef_clr);
+      DEBUG_PRINTF("Unprocessed Breaks: %u\n\n", extra_break);
+
+      DEBUG_PRINTF("BREAK detected at %lu\n", break_start_time);
+      DEBUG_PRINTF("Break Width: %lu us\n", (unsigned long)break_width_us); 
+      DEBUG_PRINTF("MAB duration: %lu us\n\n", mab_duration);
+      DEBUG_PRINTF("Data Couter: %ld\n", data_couter);
+      DEBUG_PRINTF(" - Red: %02X \n - Green: %02X \n - Blue: %02X \n - White: %02X\n", 
+        received_frame.data[0], received_frame.data[1], 
+        received_frame.data[2], received_frame.data[3]);
+      DEBUG_PRINTF("First 5 DMX bytes: ");
+      for (int i = 0; i < 5; i++) {
+        DEBUG_PRINTF("%02X ", dmx_frame[i]);
+      }
       DEBUG_PRINTF("\n");
-      // DEBUG_PRINTF("--------------------------\n");
-      // }
+      DEBUG_PRINTF("--------------------------\n");
+      }
       #endif
     }
   }
@@ -345,26 +277,7 @@ void DMX_RX_Task_Init() {
   xTaskCreatePinnedToCore(dmx_connection_check_task,
                           "dmx_connection_check_task", 4096, NULL, 1, NULL, 0);
 #ifdef Print_DMX
-  xTaskCreatePinnedToCore(print_dmx_data, "print_dmx_data", 2048, NULL, 1, NULL,
+  xTaskCreatePinnedToCore(process_dmx_data_task, "process_dmx_data_task", 2048, NULL, 1, NULL,
                           0);
 #endif
-}
-
-// ------- Process DMX Frame -------
-void process_dmx_frame(uint8_t *frame, int len) {
-  ESP_LOGI(TAG,
-           "DMX frame received len=%d start=0x%02X ch1=%u ch2=%u ch3=%u ch4=%u",
-           len, frame[0], frame[1], frame[2], frame[3], frame[4]);
-}
-
-// ------- log Buffer Hex -------
-void logBufferHex(const uint8_t *data, size_t len) {
-
-  DEBUG_PRINTF("Start Byte: %02X\n", data[0]);
-  DEBUG_PRINTF("Data: ");
-  for (size_t i = 1; i < len; i++) {
-    DEBUG_PRINTF("%02X ", data[i]);
-    DEBUG_PRINTF(" ");
-  }
-  DEBUG_PRINTF("\n");
 }
